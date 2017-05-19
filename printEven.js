da472fc8177399d081fff8dd32743b4ebe9f010b
smchow <sunita.m.chowdhury@gmail.com>
2017-05-18 22:44:53 -0400

    Initial files

---
+++ printEven.js
@@ -0,0 +1,11 @@
+var inputNums = [3, 10]
+
+function printEven(iNums){
+	var retNums = [];
+	for (var i= iNums[0] , i < iNums[0], i++ ){
+		console.log(i);
+		return retNums;
+	}
+}
+
+var output = printEven(inputNums);
