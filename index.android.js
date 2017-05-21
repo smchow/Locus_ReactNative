df21f85491e7efa12a602ed7bb6881e99f7192ab
smchow <sunita.m.chowdhury@gmail.com>
2017-05-20 21:43:18 -0400

    added projects view

--- index.android.js
+++ index.android.js
@@ -9,22 +9,84 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View , ListView , Image,  TouchableHighlight
 } from 'react-native';
 
 export default class Locus_ReactNative extends Component {
+   constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      projects: ds.cloneWithRows([]),
+      notes: ds.cloneWithRows([]),
+      studentId: "3",
+      showProjects: true,
+      loggedin: false,
+
+    };
+  }
+  componentDidMount(){
+    console.log("test")
+    let url = "https://react-locus.herokuapp.com/view/" + this.state.studentId ;
+    fetch(url)
+    .then(res => res.json())
+    .then(projects => {
+      console.log(projects[1]);
+      this.setState({
+        projects : this.state.projects.cloneWithRows(projects)
+      })
+    })
+  }
   render() {
+     let showProjects = null;
+    let showLogin = null;
+   if (this.state.showProjects){
+      showProjects = 
+
+       <ListView
+          dataSource={this.state.projects}
+          renderRow = {
+              (rowData) => (
+              <View style={{flex: 1, 
+              flexDirection: 'row', 
+              paddingTop: 5,
+              borderColor: 'black', 
+              borderStyle: 'solid',
+              borderWidth: 1,
+              }}>
+                  <Image style={{width: 100, height: 100, justifyContent:'flex-start'}}
+                   source={{uri: rowData.image_url}}
+                  />
+            
+         
+                  <Text style= {styles.listHeaderItem}> Project: {rowData.name} {"\n"}{"\n"}
+                  <Text style = {styles.listItem}> Description: {rowData.tagLine}{"\n"}
+                        Announcements: {rowData.current_announcements}{"\n"}
+                  </Text>
+                  
+                  </Text>
+                  <Text > {"\n"}{"\n"}</Text>
+                  
+              
+              </View>
+             
+
+              )
+
+          }
+        //listview
+        /> 
+    }
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to Locus React Native!
+          Welcome to React Native!
         </Text>
+          {showProjects}
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
         </Text>
       </View>
     );
